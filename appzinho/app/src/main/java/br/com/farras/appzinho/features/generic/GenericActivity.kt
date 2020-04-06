417f7b0d17df3f680c84fbf01970747a1201ea75
Paulo Ferreira <ice_pjesus@uolinc.com.br>
2020-04-05 23:48:47 -0300

    Ajustando generic activity

--- appzinho/app/src/main/java/br/com/farras/appzinho/features/generic/GenericActivity.kt
+++ appzinho/app/src/main/java/br/com/farras/appzinho/features/generic/GenericActivity.kt
@@ -9,10 +9,15 @@ class GenericActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generic)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
 
         setupView()
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return super.onSupportNavigateUp()
+    }
     private fun setupView() {
         val title = intent?.getStringExtra("title")
 
