8835f7a850da15ceeba2991de9a2c806c65fc764
Paulo Ferreira <bale_paulo@Outlook.com>
2020-05-03 23:51:07 -0300

    Items AC03

--- appzinho/app/src/main/java/br/com/farras/appzinho/features/main/MainActivity.kt
+++ appzinho/app/src/main/java/br/com/farras/appzinho/features/main/MainActivity.kt
@@ -7,27 +7,37 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.farras.appzinho.R
 import br.com.farras.appzinho.features.generic.GenericActivity
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.support.v4.drawerLayout
 import org.jetbrains.anko.toast
+import org.jetbrains.anko.toolbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        setSupportActionBar(toolbar)
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         setupEvents()
         setupView()
+        setupNavigationDrawer()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -64,6 +74,34 @@ class MainActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_action_my_events -> showGenericActivity("Meus rolês")
+                R.id.item_action_register_event -> showGenericActivity("Cadastrar rolê")
+            R.id.item_action_update_event -> showGenericActivity("Atualizar rolê")
+            R.id.item_action_logout -> finish()
+        }
+
+//        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun setupNavigationDrawer() {
+        supportActionBar?.hide()
+        var toogle = ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+
+        drawer_layout.addDrawerListener(toogle)
+        toogle.syncState()
+
+        drawer_menu.setNavigationItemSelectedListener(this)
     }
 
     private fun showProgressBar() {
