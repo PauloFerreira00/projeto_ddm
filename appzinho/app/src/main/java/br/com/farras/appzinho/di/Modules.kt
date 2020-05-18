859de5a86219c80e71bd3cf690cfb047e7d3f66e
Paulo Ferreira <bale_paulo@Outlook.com>
2020-05-18 00:10:27 -0300

    Updating modules

--- appzinho/app/src/main/java/br/com/farras/appzinho/di/Modules.kt
+++ appzinho/app/src/main/java/br/com/farras/appzinho/di/Modules.kt
@@ -1,10 +1,13 @@
 package br.com.farras.appzinho.di
 
+import androidx.room.Room
+import br.com.farras.appzinho.database.AppDatabase
 import br.com.farras.appzinho.features.login.LoginViewModel
 import br.com.farras.appzinho.features.main.MainViewModel
 import br.com.farras.appzinho.features.register.RegisterViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import org.koin.android.ext.koin.androidContext
 import org.koin.android.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
@@ -15,10 +18,18 @@ object Modules {
 
         viewModel { RegisterViewModel(get(name = "FirebaseFirestore")) }
 
-        viewModel { MainViewModel(get(name = "FirebaseFirestore")) }
+        viewModel { MainViewModel(get(name = "FirebaseFirestore"), get(name = "appDatabase")) }
     }
 
     private val general = module {
+
+        single("appDatabase") {
+            Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database.db")
+                .build()
+        }
+
+        single { get<AppDatabase>().eventDao() }
+
         single(name = "FirebaseAuth") { FirebaseAuth.getInstance() }
 
         single(name = "FirebaseFirestore") { FirebaseFirestore.getInstance() }
