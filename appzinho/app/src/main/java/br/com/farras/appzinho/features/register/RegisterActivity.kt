d8480f1b5ce9615f7e82d58a609fd29fcd265a9b
Paulo Ferreira <bale_paulo@Outlook.com>
2020-05-17 23:00:07 -0300

    Creating register activity

--- appzinho/app/src/main/java/br/com/farras/appzinho/features/register/RegisterActivity.kt
+++ appzinho/app/src/main/java/br/com/farras/appzinho/features/register/RegisterActivity.kt
@@ -2,12 +2,48 @@ package br.com.farras.appzinho.features.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import br.com.farras.appzinho.R
+import br.com.farras.appzinho.features.login.LoginViewModel
+import br.com.farras.appzinho.models.Event
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.toast
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val viewModel: RegisterViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        title = "Cadastrar rolê"
+
+        setupActions()
+    }
+
+    private fun setupActions() {
+       bt_register_event_data.setOnClickListener {
+           if (et_event_name.text.toString().isNotEmpty() &&
+               et_evente_date.text.toString().isNotEmpty()
+           ) {
+               val event = Event(et_event_name.text.toString(), et_evente_date.text.toString())
+               register(event)
+           } else {
+               toast("Preencha todos os campos!")
+           }
+
+       }
+    }
+
+    private fun register(event: Event) {
+        viewModel.register(event).observe(this, Observer { result ->
+            if (result.success != null) {
+                toast(result.success)
+                finish()
+            } else {
+                result.failure?.message?.let { toast(it) }
+            }
+        })
     }
 }
---
+++ appzinho/app/src/main/java/br/com/farras/appzinho/features/register/RegisterViewModel.kt
@@ -0,0 +1,30 @@
+package br.com.farras.appzinho.features.register
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import br.com.farras.appzinho.models.Error
+import br.com.farras.appzinho.models.Event
+import com.google.firebase.firestore.FirebaseFirestore
+import br.com.farras.appzinho.models.Result
+
+class RegisterViewModel(private val firestore: FirebaseFirestore): ViewModel() {
+
+    fun register(event: Event): LiveData<Result<String>> {
+        val liveData = MutableLiveData<Result<String>>()
+        val event = hashMapOf(
+            "name" to event.name,
+            "date" to event.date
+        )
+
+        firestore.collection("events")
+            .add(event as Map<String, Any>)
+            .addOnSuccessListener { liveData.value =
+                Result(success = "Cadastro realizado com sucesso!")
+            }
+            .addOnFailureListener { liveData.value = Result(failure = Error(message = "Problema ao realizar cadastro, tente novamente!")) }
+
+        return liveData
+    }
+
+}
