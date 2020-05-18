fd7bc90272cd3ab00e70cc26dbdcf6d9875d62c2
Paulo Ferreira <bale_paulo@Outlook.com>
2020-05-18 00:10:15 -0300

    Creating event dao

---
+++ appzinho/app/src/main/java/br/com/farras/appzinho/dao/EventDao.kt
@@ -0,0 +1,23 @@
+package br.com.farras.appzinho.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import br.com.farras.appzinho.models.Event
+
+@Dao
+interface EventDao {
+    @Query("SELECT * FROM event")
+    fun getAll(): List<Event>
+
+    @Insert
+    @JvmSuppressWildcards
+    fun createAll(objects: List<Event>)
+
+    @Query("DELETE FROM event")
+    fun deleteAll()
+
+    @Delete
+    fun delete(user: Event)
+}
