3bee603364433be6fbe232a48f9daa26893124c1
Paulo Ferreira <bale_paulo@Outlook.com>
2020-05-18 00:12:26 -0300

    Updating event model

--- appzinho/app/src/main/java/br/com/farras/appzinho/models/Event.kt
+++ appzinho/app/src/main/java/br/com/farras/appzinho/models/Event.kt
@@ -1,9 +1,14 @@
 package br.com.farras.appzinho.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.firebase.firestore.PropertyName
 import java.io.Serializable
 
+@Entity
 data class Event(
-    @PropertyName("name") val name: String = "",
-    @PropertyName("date") val date: String = "-"
+    @PrimaryKey(autoGenerate = true) val uid: Int = 0,
+    @ColumnInfo @PropertyName("name") val name: String = "",
+    @ColumnInfo @PropertyName("date") val date: String = "-"
 ): Serializable
