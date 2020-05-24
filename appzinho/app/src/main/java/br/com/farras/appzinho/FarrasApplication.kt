b85134f7de3ca3443e76544d7290ae10e5508e11
Paulo Ferreira <bale_paulo@Outlook.com>
2020-05-24 15:35:07 -0300

    Adding appContext single

--- appzinho/app/src/main/java/br/com/farras/appzinho/FarrasApplication.kt
+++ appzinho/app/src/main/java/br/com/farras/appzinho/FarrasApplication.kt
@@ -1,15 +1,21 @@
 package br.com.farras.appzinho
 
 import android.app.Application
+import android.content.Context
 import br.com.farras.appzinho.di.Modules
 import com.google.firebase.FirebaseApp
 import org.koin.android.ext.android.startKoin
 
 class FarrasApplication: Application() {
 
+    companion object {
+        lateinit var appContext: Context
+    }
+
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this@FarrasApplication)
         startKoin(androidContext = this@FarrasApplication, modules = Modules.all)
+        appContext = applicationContext
     }
 }
